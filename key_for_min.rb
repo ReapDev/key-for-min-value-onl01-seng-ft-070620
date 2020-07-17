fa8c22545ffb0f9d9473aaebfa84b67dbbbec4a5
Brandon Williams <nis350ztt@gmail.com>
2020-07-17 23:28:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@ def key_for_min_value(hash)
   lowest_key = NIL
   lowest_value = nil 
   hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
   hash.each_value do |num| 
     num <=> num 
     return hash
