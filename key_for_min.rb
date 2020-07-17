c555ed5ade3601fea1d5509cd575ba85ffe41fc5
Brandon Williams <nis350ztt@gmail.com>
2020-07-17 23:25:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
-def key_for_min_value(name_hash)
-  name_hash.each_value do |num| 
+def key_for_min_value(hash)
+  
+  hash.each_value do |num| 
     num <=> num 
-    return name_hash
+    return hash
   end
 end 
